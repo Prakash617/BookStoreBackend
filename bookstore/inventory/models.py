4767512c6eed343a41828cb79bfa5700f05a526a
suman-pokharel <sumanpokharel8@gmail.com>
2024-03-28 22:31:11 +0545

    chnages

--- bookstore/inventory/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-# sold qty
-# qty
-# purchase unit price
-# product
--- bookstore/website/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-
-admin.site.register(Carousal)
-admin.site.register(Careers)
-admin.site.register(Faqs)
-admin.site.register(Menus)
