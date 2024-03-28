adf428f53ab1b42c43a17663431434dd555f2f9f
suman-pokharel <sumanpokharel8@gmail.com>
2024-03-28 22:21:57 +0545

    chnages

---
+++ bookstore/order/order_data.py
@@ -0,0 +1,23 @@
+# {
+#     "coupon":"abc",
+#     "shipping_charge": 100,
+#     "tax":10,
+#     "order_qty": [
+#         {"product":1,
+#          "quantity":5
+#         },
+#         {"product":2,
+#          "quantity":5
+#         }
+        
+#     ],
+#     "customer_address":{
+#         "full_name": "suman pokharel",
+#         "email":"sumanpokharel8@gmail.com",
+#         "phone": "123456777",
+#         "address_line_1": "jgfkhdfh hdfjs",
+#         "address_line_2": "gjdgg hsdkhskdhk",
+#         "city": "kathmandu",
+#         "country":"Nepal"
+#     }
+# }
